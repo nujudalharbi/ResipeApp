11660af01fff1d71ca5e0751f330b6b6cc96fd0a
nujudalharbi <Njoud.Aali.med.s@tuwaiq.edu.sa>
2022-09-27 04:53:18 +0300

    complete code the addRecipeView

--- RecipeApp/AddRecipeView.swift
+++ RecipeApp/AddRecipeView.swift
@@ -15,6 +15,8 @@ struct AddRecipeView: View {
     @State private var ingredients : String = ""
     @State private var directions : String = ""
     
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         NavigationView{
             Form{
@@ -30,28 +32,56 @@ struct AddRecipeView: View {
                         ForEach(Category.allCases){
                             category in
                             Text(category.rawValue)
+                                .tag(category)
+                            
                             
                         }
                     }
+                    .pickerStyle(.menu)
                     
                 }
                 Section(header: Text("Description")){
-                    
+                    TextEditor(text: $description)
                     
                     
                 }
                 Section(header: Text("Ingredients")){
-                    
+                    TextEditor(text: $ingredients)
                     
                     
                 }
                 Section(header: Text("Directions")){
                     
-                    
+                    TextEditor(text: $directions)
                     
                 }
                 
             }
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                        
+                    } label: {
+                        Label("Cancel", systemImage: "xmark")
+                            .labelStyle(.iconOnly)
+                    }
+
+                }
+                
+                ToolbarItem{
+                    Button {
+                        
+                    } label: {
+                        Label("Done", systemImage: "checkmark")
+                            .labelStyle(.iconOnly)
+                    }
+                    .disabled(name.isEmpty)
+
+                }
+                
+                
+            })
             .navigationTitle("New Recipe")
             
             .navigationBarTitleDisplayMode(.inline)
